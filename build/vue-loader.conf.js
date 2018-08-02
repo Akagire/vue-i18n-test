99fa205347adf4980cf6d30b27314e632dd599a9
Takuya Eguchi (Akagire) <Akagire@users.noreply.github.com>
2018-08-02 14:46:17 +0900

    Update vue-loader.conf.js

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -18,5 +18,13 @@ module.exports = {
     source: 'src',
     img: 'src',
     image: 'xlink:href'
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('i18n')
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use('i18n')
+      .loader('@kazupon/vue-i18n-loader')
   }
 }
